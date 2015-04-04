1776b2026ca19c5c3fca69af0973fcccaaf22035
Aaron Bradley <abeats20@gmail.com>
2015-04-03 19:29:55 -0500

    Added a search bar and table view. The search puts fake results in the table for now.

---
+++ ab-StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  ab-StoreSearch
+//
+//  Created by Aaron Bradley on 4/3/15.
+//  Copyright (c) 2015 Aaron Bradley. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
