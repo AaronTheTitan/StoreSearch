487cad8ba00072fabc4c905c1fc1a375ae1f35f8
Aaron Bradley <abeats20@gmail.com>
2015-04-05 17:35:27 -0500

    finished NSURLSession feature

--- ab-StoreSearch/SearchResultCell.swift
+++ ab-StoreSearch/SearchResultCell.swift
@@ -14,6 +14,11 @@ class SearchResultCell: UITableViewCell {
     @IBOutlet weak var artistNameLabel: UILabel!
     @IBOutlet weak var artworkImageView: UIImageView!
 
+    var downloadTask: NSURLSessionDownloadTask?
+
+
+// MARK: - BEGIN CODE -------------------------------------------- //
+
     override func awakeFromNib() {
         super.awakeFromNib()
 
@@ -26,6 +31,55 @@ class SearchResultCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+
+    func configureForSearchResult(searchResult: SearchResult) {
+        nameLabel.text = searchResult.name
+
+        if searchResult.artistName.isEmpty {
+            artistNameLabel.text = "Unknown"
+        } else {
+            artistNameLabel.text = String(format: "%@ (%@)", searchResult.artistName, kindForDisplay(searchResult.kind))
+        }
+
+        artworkImageView.image = UIImage(named: "Placeholder")
+        if let url = NSURL(string: searchResult.artworkURL60) {
+            downloadTask = artworkImageView.loadImageWithURL(url)
+        }
+    }
+
+    // cancels image download still in progress when it doesn't need to be
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+        downloadTask?.cancel()
+        downloadTask = nil
+
+        nameLabel.text = nil
+        artistNameLabel.text = nil
+        artworkImageView.image = nil
+
+        println("got dat suckca")
+    }
+
+
+// MARK: - ADJUST NAMES FOR KIND ----------------------------------------------- //
+
+    func kindForDisplay(kind: String) -> String {
+
+        switch kind {
+        case "album": return "Album"
+        case "audiobook": return "Audio Book"
+        case "book": return "Book"
+        case "ebook": return "E-Book"
+        case "feature-movie": return "Movie"
+        case "music-video": return "Music Video"
+        case "podcast": return "Podcast"
+        case "software": return "App"
+        case "song": return "Song"
+        case "tv-episode": return "TV Episode"
+        default: return kind
+        }
     }
 
 }
---
+++ ab-StoreSearch/UIImageView+DownloadImage.swift
@@ -0,0 +1,36 @@
+//
+//  UIImageView+DownloadImage.swift
+//  ab-StoreSearch
+//
+//  Created by Aaron Bradley on 4/5/15.
+//  Copyright (c) 2015 Aaron Bradley. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+
+    func loadImageWithURL(url: NSURL) -> NSURLSessionDownloadTask {
+        let session = NSURLSession.sharedSession()
+
+        let downloadTask = session.downloadTaskWithURL(url, completionHandler: {
+            [weak self] url, response, error in
+
+            if error == nil && url != nil {
+                if let data = NSData(contentsOfURL: url) {
+                    if let image = UIImage(data: data) {
+                        dispatch_async(dispatch_get_main_queue()) {
+                            if let strongSelf = self {
+                                strongSelf.image = image
+                            }
+                        }
+                    }
+                }
+            }
+        })
+
+        downloadTask.resume()
+        return downloadTask
+    }
+
+}
